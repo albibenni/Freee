c2ebca8b4e12eb9bdd8916d74325961905468df6
Alberto Benatti <albi.benni8@gmail.com>
2026-04-04 20:09:04 +0200

    init 2

---
+++ Sources/Freee/FreeeApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+#if !SWIFT_PACKAGE
+    @main
+    struct FreeeApp: App {
+        var body: some Scene {
+            WindowGroup {
+                ContentView()
+            }
+        }
+    }
+#endif
